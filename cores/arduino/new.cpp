c6ce36d56cd5e874e60c3ba8a36cc77c307df718
Pharap <2933055+Pharap@users.noreply.github.com>
2018-08-18 09:09:15 +0100

    Add placement new operator

--- cores/arduino/new.cpp
+++ cores/arduino/new.cpp
@@ -26,6 +26,10 @@ void *operator new[](size_t size) {
   return malloc(size);
 }
 
+void * operator new(size_t size, void * ptr) {
+  return ptr;
+}
+
 void operator delete(void * ptr) {
   free(ptr);
 }
