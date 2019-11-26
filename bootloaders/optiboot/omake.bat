0e48ff8e862db6a89116979d1421b4c8b966265d
Armin <armin.arduino@gmail.com>
2019-11-26 20:41:03 +0100

    New make.exe path for version 1.8.10

--- bootloaders/optiboot/omake.bat
+++ bootloaders/optiboot/omake.bat
@@ -1,1 +1,3 @@
-..\..\..\tools\avr\utils\bin\make OS=windows ENV=arduino %*
+rem ..\..\..\tools\avr\utils\bin\make OS=windows ENV=arduino %*
+REM New make.exe path for version 1.8.10
+%userprofile%\AppData\Local\Arduino15\packages\arduino\tools\avr-gcc\avr-gcc-9.1.0-x64-mingw\bin\make OS=windows ENV=arduino %*
