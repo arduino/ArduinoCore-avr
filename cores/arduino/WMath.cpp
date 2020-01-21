c265ddb784d08d7acf4c26beb585527cd40534c7
Dirk O. Kaar <dok@dok-net.net>
2020-01-21 12:40:32 +0100

    Greatly reduces error rate (half, or 0 zero errors, depends on in/out ranges) for round-trip mapping at the same performance.
    (Based on "improved_map" from ESP8266's Servo.cpp)

--- cores/arduino/WMath.cpp
+++ cores/arduino/WMath.cpp
@@ -49,9 +49,12 @@ long random(long howsmall, long howbig)
   return random(diff) + howsmall;
 }
 
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  const long dividend = out_max - out_min;
+  const long divisor = in_max - in_min;
+  const long delta = x - in_min;
+
+  return (delta * dividend + (divisor / 2)) / divisor + out_min;
 }
 
 unsigned int makeWord(unsigned int w) { return w; }
