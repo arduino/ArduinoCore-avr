ff655be9c17c9a68d0faead439ea25bf1a502558
thekunalsaini <43953836+thekunalsaini@users.noreply.github.com>
2020-02-29 16:52:23 +0530

    to avoid out of bound error

--- libraries/EEPROM/examples/eeprom_crc/eeprom_crc.ino
+++ libraries/EEPROM/examples/eeprom_crc/eeprom_crc.ino
@@ -34,7 +34,7 @@ void loop() {
 
 unsigned long eeprom_crc(void) {
 
-  const unsigned long crc_table[16] = {
+  const unsigned long crc_table[17] = {
     0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
     0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
     0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
