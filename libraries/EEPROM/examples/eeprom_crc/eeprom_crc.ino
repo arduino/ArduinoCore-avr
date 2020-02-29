d2bf895e2bf1ec826a03ff517eceb5e2f575f9d2
thekunalsaini <43953836+thekunalsaini@users.noreply.github.com>
2020-03-01 00:09:31 +0530

    New changes
    Create a out of bound condition in for loop

--- libraries/EEPROM/examples/eeprom_crc/eeprom_crc.ino
+++ libraries/EEPROM/examples/eeprom_crc/eeprom_crc.ino
@@ -34,16 +34,22 @@ void loop() {
 
 unsigned long eeprom_crc(void) {
 
-  const unsigned long crc_table[17] = {
+  const unsigned long crc_table[16] = {
     0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
     0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
     0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
     0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
   };
-
+  
   unsigned long crc = ~0L;
 
   for (int index = 0 ; index < EEPROM.length()  ; ++index) {
+    if(index > 16)
+    {
+     Serial.print("\nOut of Bound!");
+     break;
+    }
+     
     crc = crc_table[(crc ^ EEPROM[index]) & 0x0f] ^ (crc >> 4);
     crc = crc_table[(crc ^ (EEPROM[index] >> 4)) & 0x0f] ^ (crc >> 4);
     crc = ~crc;
