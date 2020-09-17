b521738f65bc5b7f93dad142f89a66e61179f8a1
Matthijs Kooijman <matthijs@stdin.nl>
2020-09-17 21:53:11 +0200

    Wire: Enable a timeout by default
    When no timeout is explicitly configured, Wire now uses a timeout of
    25ms, resetting the Wire hardware if a timeout occurs.
    
    The timeout length matches the timeout you get when you call
    `Wire.setWireTimeout()` without arguments, and is loosely based on the
    SMBus timeout and maximum clock stretching time (though it works quite
    differently) and is rather long. In practice, this means that even if
    another master is on the bus, or slaves are using significant clock
    stretching, the timeout will probably not trigger unless there really is
    a lockup.
    
    This also enables a reset of the Wire hardware on a timeout (unlike
    `Wire.setWireTimeout()` without arguments), under the assumption that if
    a sketch has not set up timeout settings explicitly, it probably just
    wants things to keep running as well as possible and resetting allows
    recovering from most transient lockups.
    
    See #42 for earlier discussion of this.

--- libraries/Wire/src/Wire.h
+++ libraries/Wire/src/Wire.h
@@ -35,8 +35,8 @@
 #define WIRE_HAS_TIMEOUT 1
 
 // When not configured, these settings are used for the timeout
-#define WIRE_DEFAULT_TIMEOUT 0
-#define WIRE_DEFAULT_RESET_WITH_TIMEOUT false
+#define WIRE_DEFAULT_TIMEOUT 25000
+#define WIRE_DEFAULT_RESET_WITH_TIMEOUT true
 
 class TwoWire : public Stream
 {
