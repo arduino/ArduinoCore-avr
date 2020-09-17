66d06b033c3f6eafde901418be3c089ffcc6ebfc
Matthijs Kooijman <matthijs@stdin.nl>
2020-09-17 17:40:02 +0200

    Call std::terminate on pure or deleted virtual functions
    These are special functions that are presumably put into vtables for
    deleted or pure virtual functions. Previously, this would call `abort()`
    directly, but calling `std::terminate()` achieves the same effect, but
    allows user code to change the behavior (e.g. to print to serial, blink
    leds or whatever makes sense).

--- cores/arduino/abi.cpp
+++ cores/arduino/abi.cpp
@@ -28,14 +28,9 @@ namespace std {
 }
 
 void __cxa_pure_virtual(void) {
-  // We might want to write some diagnostics to uart in this case
-  //std::terminate();
-  abort();
+  std::terminate();
 }
 
 void __cxa_deleted_virtual(void) {
-  // We might want to write some diagnostics to uart in this case
-  //std::terminate();
-  abort();
+  std::terminate();
 }
-
