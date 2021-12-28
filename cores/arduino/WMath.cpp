8ec190d8507df5408e6d8adbb40ce1140fd74545
Fermín Olaiz <ferminolaiz@gmail.com>
2021-12-28 17:23:47 -0300

    Add support for different types in map() function (templates)

--- cores/arduino/WMath.cpp
+++ cores/arduino/WMath.cpp
@@ -49,10 +49,5 @@ long random(long howsmall, long howbig)
   return random(diff) + howsmall;
 }
 
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 unsigned int makeWord(unsigned int w) { return w; }
 unsigned int makeWord(unsigned char h, unsigned char l) { return (h << 8) | l; }
