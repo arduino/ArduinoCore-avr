6df55d10bd4b57022c72f2cbf54dc0d088578c81
natitr <trevor.natiuk@wallawalla.edu>
2022-02-22 16:13:54 -0800

    Move definition of INADDR_NONE to IPAddress.cpp

--- cores/arduino/IPAddress.cpp
+++ cores/arduino/IPAddress.cpp
@@ -112,3 +112,5 @@ size_t IPAddress::printTo(Print& p) const
     return n;
 }
 
+const IPAddress INADDR_NONE(0,0,0,0);
+
--- cores/arduino/IPAddress.h
+++ cores/arduino/IPAddress.h
@@ -73,6 +73,6 @@ public:
     friend class DNSClient;
 };
 
-const IPAddress INADDR_NONE(0,0,0,0);
+extern const IPAddress INADDR_NONE;
 
 #endif
