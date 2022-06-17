5806b17e0b619a3b5751889886eeb992602b6af3
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2022-06-17 19:24:20 +0200

    Layout, indents and removed compiler warning.

--- cores/arduino/Print.cpp
+++ cores/arduino/Print.cpp
@@ -267,30 +267,30 @@ size_t Print::printFloat(double number, uint8_t digits)
 
 size_t Print::printf(const char *format, ...)
 {
-    char loc_buf[64];
-    char * temp = loc_buf;
-    va_list arg;
-    va_list copy;
-    va_start(arg, format);
-    va_copy(copy, arg);
-    int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
-    va_end(copy);
-    if(len < 0) {
-        va_end(arg);
-        return 0;
-    };
-    if(len >= sizeof(loc_buf)){
-        temp = (char*) malloc(len+1);
-        if(temp == NULL) {
-            va_end(arg);
-            return 0;
-        }
-        len = vsnprintf(temp, len+1, format, arg);
-    }
+  char loc_buf[64];
+  char* temp = loc_buf;
+  va_list arg;
+  va_list copy;
+  va_start(arg, format);
+  va_copy(copy, arg);
+  int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
+  va_end(copy);
+  if (len < 0) {
     va_end(arg);
-    len = write((uint8_t*)temp, len);
-    if(temp != loc_buf){
-        free(temp);
+    return 0;
+  }
+  if (len >= (int)sizeof(loc_buf)) {
+    temp = (char*)malloc(len + 1);
+    if (temp == NULL) {
+      va_end(arg);
+      return 0;
     }
-    return len;
+    len = vsnprintf(temp, len + 1, format, arg);
+  }
+  va_end(arg);
+  len = write((uint8_t*)temp, len);
+  if (temp != loc_buf) {
+    free(temp);
+  }
+  return len;
 }
