9cc9ada37bfc2746642f516f1e554f5285658935
dac1e <wolfgang@die-schmieders.de>
2024-12-28 07:46:19 +0100

    Changed static variables of Wire to non static. Some functions only doing type casts and forwarding are now inline which improves speed. Refactored code to more actual C++ style.

--- libraries/Wire/src/Wire.cpp
+++ libraries/Wire/src/Wire.cpp
@@ -28,24 +28,18 @@
 
 #include "Wire.h"
 
-// Initialize Class Variables //////////////////////////////////////////////////
-
-uint8_t TwoWire::rxBuffer[BUFFER_LENGTH];
-uint8_t TwoWire::rxBufferIndex = 0;
-uint8_t TwoWire::rxBufferLength = 0;
-
-uint8_t TwoWire::txAddress = 0;
-uint8_t TwoWire::txBuffer[BUFFER_LENGTH];
-uint8_t TwoWire::txBufferIndex = 0;
-uint8_t TwoWire::txBufferLength = 0;
-
-uint8_t TwoWire::transmitting = 0;
-void (*TwoWire::user_onRequest)(void);
-void (*TwoWire::user_onReceive)(int);
 
 // Constructors ////////////////////////////////////////////////////////////////
 
 TwoWire::TwoWire()
+  : rxBufferIndex(0)
+  , rxBufferLength(0)
+  , txAddress(0)
+  , txBufferIndex(0)
+  , txBufferLength(0)
+  , transmitting(0)
+  , user_onRequest(nullptr)
+  , user_onReceive(nullptr)
 {
 }
 
@@ -60,19 +54,14 @@ void TwoWire::begin(void)
   txBufferLength = 0;
 
   twi_init();
-  twi_attachSlaveTxEvent(onRequestService); // default callback must exist
-  twi_attachSlaveRxEvent(onReceiveService); // default callback must exist
+  twi_attachSlaveTxEvent(onRequestService_); // default callback must exist
+  twi_attachSlaveRxEvent(onReceiveService_); // default callback must exist
 }
 
 void TwoWire::begin(uint8_t address)
 {
   begin();
   twi_setAddress(address);
-}
-
-void TwoWire::begin(int address)
-{
-  begin((uint8_t)address);
 }
 
 void TwoWire::end(void)
@@ -164,25 +153,6 @@ uint8_t TwoWire::requestFrom(uint8_t address, uint8_t quantity, uint32_t iaddres
   return read;
 }
 
-uint8_t TwoWire::requestFrom(uint8_t address, uint8_t quantity, uint8_t sendStop) {
-	return requestFrom((uint8_t)address, (uint8_t)quantity, (uint32_t)0, (uint8_t)0, (uint8_t)sendStop);
-}
-
-uint8_t TwoWire::requestFrom(uint8_t address, uint8_t quantity)
-{
-  return requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)true);
-}
-
-uint8_t TwoWire::requestFrom(int address, int quantity)
-{
-  return requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)true);
-}
-
-uint8_t TwoWire::requestFrom(int address, int quantity, int sendStop)
-{
-  return requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)sendStop);
-}
-
 void TwoWire::beginTransmission(uint8_t address)
 {
   // indicate that we are transmitting
@@ -192,11 +162,6 @@ void TwoWire::beginTransmission(uint8_t address)
   // reset tx buffer iterator vars
   txBufferIndex = 0;
   txBufferLength = 0;
-}
-
-void TwoWire::beginTransmission(int address)
-{
-  beginTransmission((uint8_t)address);
 }
 
 //
@@ -318,6 +283,11 @@ void TwoWire::flush(void)
   // XXX: to be implemented.
 }
 
+void TwoWire::onReceiveService_(uint8_t* inBytes, int numBytes)
+{
+  Wire.onReceiveService(inBytes, numBytes);
+}
+
 // behind the scenes function that is called when data is received
 void TwoWire::onReceiveService(uint8_t* inBytes, int numBytes)
 {
@@ -341,6 +311,11 @@ void TwoWire::onReceiveService(uint8_t* inBytes, int numBytes)
   rxBufferLength = numBytes;
   // alert user program
   user_onReceive(numBytes);
+}
+
+void TwoWire::onRequestService_()
+{
+  Wire.onRequestService();
 }
 
 // behind the scenes function that is called when data is requested
--- libraries/Wire/src/Wire.h
+++ libraries/Wire/src/Wire.h
@@ -20,6 +20,9 @@
   Modified 2020 by Greyson Christoforo (grey@christoforo.net) to implement timeouts
 */
 
+
+#pragma once
+
 #ifndef TwoWire_h
 #define TwoWire_h
 
@@ -34,52 +37,67 @@
 class TwoWire : public Stream
 {
   private:
-    static uint8_t rxBuffer[];
-    static uint8_t rxBufferIndex;
-    static uint8_t rxBufferLength;
+    uint8_t rxBuffer[BUFFER_LENGTH];
+    uint8_t rxBufferIndex;
+    uint8_t rxBufferLength;
 
-    static uint8_t txAddress;
-    static uint8_t txBuffer[];
-    static uint8_t txBufferIndex;
-    static uint8_t txBufferLength;
+    uint8_t txAddress;
+    uint8_t txBuffer[BUFFER_LENGTH];
+    uint8_t txBufferIndex;
+    uint8_t txBufferLength;
 
-    static uint8_t transmitting;
-    static void (*user_onRequest)(void);
-    static void (*user_onReceive)(int);
-    static void onRequestService(void);
-    static void onReceiveService(uint8_t*, int);
+    uint8_t transmitting;
+    void (*user_onRequest)(void);
+    void (*user_onReceive)(int);
+
+    static void onRequestService_(void);
+    static void onReceiveService_(uint8_t*, int);
+
+    void onRequestService(void);
+    void onReceiveService(uint8_t*, int);
   public:
     TwoWire();
     void begin();
     void begin(uint8_t);
-    void begin(int);
+    inline void begin(int address) {begin(static_cast<uint8_t>(address));}
     void end();
     void setClock(uint32_t);
     void setWireTimeout(uint32_t timeout = 25000, bool reset_with_timeout = false);
     bool getWireTimeoutFlag(void);
     void clearWireTimeoutFlag(void);
     void beginTransmission(uint8_t);
-    void beginTransmission(int);
+    void beginTransmission(int address) {beginTransmission(static_cast<uint8_t>(address));}
     uint8_t endTransmission(void);
     uint8_t endTransmission(uint8_t);
-    uint8_t requestFrom(uint8_t, uint8_t);
-    uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
     uint8_t requestFrom(uint8_t, uint8_t, uint32_t, uint8_t, uint8_t);
-    uint8_t requestFrom(int, int);
-    uint8_t requestFrom(int, int, int);
-    virtual size_t write(uint8_t);
-    virtual size_t write(const uint8_t *, size_t);
-    virtual int available(void);
-    virtual int read(void);
-    virtual int peek(void);
-    virtual void flush(void);
+    inline uint8_t requestFrom(uint8_t address, uint8_t quantity) {
+      return requestFrom(address, quantity, static_cast<uint8_t>(true));
+    }
+    inline uint8_t requestFrom(uint8_t address, uint8_t quantity, uint8_t sendStop) {
+      return requestFrom(address, quantity, static_cast<uint32_t>(0)
+          , static_cast<uint8_t>(0), static_cast<uint8_t>(sendStop));
+    }
+    inline uint8_t requestFrom(int address, int quantity) {
+      return requestFrom(static_cast<uint8_t>(address), static_cast<uint8_t>(quantity)
+          , static_cast<uint8_t>(true));
+    }
+    inline uint8_t requestFrom(int address, int quantity, int sendStop) {
+      return requestFrom(static_cast<uint8_t>(address), static_cast<uint8_t>(quantity)
+          , static_cast<uint8_t>(sendStop));
+    }
+    virtual size_t write(uint8_t) override;
+    virtual size_t write(const uint8_t *, size_t) override;
+    virtual int available(void) override;
+    virtual int read(void) override;
+    virtual int peek(void) override;
+    virtual void flush(void) override;
     void onReceive( void (*)(int) );
     void onRequest( void (*)(void) );
 
-    inline size_t write(unsigned long n) { return write((uint8_t)n); }
-    inline size_t write(long n) { return write((uint8_t)n); }
-    inline size_t write(unsigned int n) { return write((uint8_t)n); }
-    inline size_t write(int n) { return write((uint8_t)n); }
+    inline size_t write(unsigned long n) { return write(static_cast<uint8_t>(n)); }
+    inline size_t write(long n) { return write(static_cast<uint8_t>(n)); }
+    inline size_t write(unsigned int n) { return write(static_cast<uint8_t>(n)); }
+    inline size_t write(int n) { return write(static_cast<uint8_t>(n)); }
     using Print::write;
 };
 
