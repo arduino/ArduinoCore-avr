45bd1b129f7889f60a86b79f56cda111d5a1f034
Flamethrower <ignaciovallejoalcalde@gmail.com>
2025-09-28 17:38:56 +0200

    Create ArduinoDS4_USBDesc.h

---
+++ cores/arduino/ArduinoDS4/ArduinoDS4_USBDesc.h
@@ -0,0 +1,49 @@
+/*
+ *  Project     ArduinoDS4 AVR core
+ *  @author     Flamethrower
+ *  @link       github.com/Flamethr0wer/ArduinoDS4_AVR
+ *  @license    MIT - Copyright (c) 2019 Flamethrower
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#include "USBAPI.h"
+
+#ifndef ARDUINODS4_USBDESC_H
+#define ARDUINODS4_USBDESC_H
+
+#if defined(USBCON)
+
+extern const DeviceDescriptor USB_DeviceDescriptor PROGMEM;
+
+extern const u8  USB_ConfigDescriptor[] PROGMEM;
+extern const u16 USB_ConfigDescriptorSize PROGMEM;
+
+extern const u8 STRING_SERIAL[] PROGMEM;
+extern const u8 STRING_SECURITY[] PROGMEM;
+
+#define ISECURITY 4
+
+#define ARDUINODS4_TX_ENDPOINT	1
+#define ARDUINODS4_RX_ENDPOINT	2
+
+#endif
+
+#endif
