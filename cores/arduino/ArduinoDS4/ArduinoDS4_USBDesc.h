784954867118166c7eec58258370017c83fdd98c
Flamethrower <ignaciovallejoalcalde@gmail.com>
2025-10-01 18:57:27 +0200

    Update ArduinoDS4_USBDesc.h

--- cores/arduino/ArduinoDS4/ArduinoDS4_USBDesc.h
+++ cores/arduino/ArduinoDS4/ArduinoDS4_USBDesc.h
@@ -40,19 +40,19 @@ extern const u8  USB_ConfigDescriptor[] PROGMEM;
 extern const u16 USB_ConfigDescriptorSize PROGMEM;
 
 extern const u8 output_0x02[] PROGMEM;
-extern const u16 output_0x02_length PROGMEM;
+extern const u16 output_0x02_size PROGMEM;
 
 extern const u8 output_0x03[] PROGMEM;
-extern const u16 output_0x03_length PROGMEM;
+extern const u16 output_0x03_size PROGMEM;
 
 extern const u8 output_0x12[] PROGMEM;
-extern const u16 output_0x12_length PROGMEM;
+extern const u16 output_0x12_size PROGMEM;
 
 extern const u8 output_0xa3[] PROGMEM;
-extern const u16 output_0xa3_length PROGMEM;
+extern const u16 output_0xa3_size PROGMEM;
 
 extern const u8 output_0xf3[] PROGMEM;
-extern const u16 output_0xf3_length PROGMEM;
+extern const u16 output_0xf3_size PROGMEM;
 
 
 extern const u8 STRING_SERIAL[] PROGMEM;
